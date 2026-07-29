0f20b1b758726dcd8818e9b9321d0972bdadbd81
m4tatica <comercial@m4tatica.com.br>
2026-07-29 11:14:20 -0300

    Retirada de sitemap do app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -99,12 +99,6 @@ def create_app():
     def robots_at_root():
         return send_from_directory(os.path.join(app.root_path, 'static'), 'robots.txt')
 
-    @app.route('/sitemap.xml')
-    def sitemap_at_root():
-        # Redireciona para a rota que você já criou no blueprint da loja
-        from flask import url_for
-        return redirect(url_for('loja.sitemap'))
-
     # 🚀 CONFIGURAÇÃO DE CACHE DE ATIVOS (SEO & PERFORMANCE)
     @app.after_request
     def add_header(response):
